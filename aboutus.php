84a96870a2e14364e3624afa859c2427968517eb
MYITEDU <myitedu@localhosts>
2020-07-07 18:36:00 -0400

    Jon created aboutus page

---
+++ aboutus.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>About Jon Toshmatov</h1>
+<img src="https://images.youracclaim.com/images/1ebc3d57-1dcb-4918-85fe-ebde58e08ff0/jon.jpg">
+
+<style>
+    body{
+        background-color: darkred;
+        color: antiquewhite;
+        text-align: center;
+    }
+</style>
+</body>
+</html>
