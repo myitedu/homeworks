573b6547dbf923f586ab077e4a2a23d8f07d39ae
Khasan Erkinov <Khasan.7595@gmail.com>
2020-07-09 11:55:23 -0400

    khasan is saving his code

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-Jon Toshmatov: Hello, This is line 1 6:05 pm
-Jon says howdy and it is 6:21 pm
-Sardor Narzikulov 06:21 pm
-Khasan Erkinov is speaking 6:21pm
-Sardor: Hello World
-Khasan is doing homework at 6:25 pm
-Toy qachon?
-working hard
 
-
-
-sunny days
-what's up?
+learning IT
 
