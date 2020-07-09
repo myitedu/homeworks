d12d716f5276ebb4b33572670afa8ff4ec04b2cf
Khasan Erkinov <Khasan.7595@gmail.com>
2020-07-09 14:10:32 -0400

    Kasan created and updated the IT world page

--- IT world.php
+++ IT world.php
@@ -1,1 +1,17 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1> Software engineer </h1>
+<img src="https://www.enterpriseitworld.com/wp-content/uploads/2019/06/LINKS-Dropbox.jpg"
+<p> job: IT </p>
+<p> Salary : $100.000 </p>
+<p> Contract : W2 </p>
+</body>
+</html>
